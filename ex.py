919ef8db0f72a31863cd53d782aeb381ec990e80
RINONIN <779057748@qq.com>
2021-09-13 22:07:34 +0800

    Initial commit

--- ex.py
+++ ex.py
@@ -3,4 +3,19 @@
 日期: 2021年09月14日
 """
 
+<<<<<<< HEAD
 print('hello world')
+=======
+import sys
+import requests
+
+# print('hello world')
+# 命令行编程
+
+# print(sys.argv)
+# print(sys.argv[1])
+
+# 三方库引用
+response = requests.get("http://www.baidu.com")
+print(response.text)
+>>>>>>> a50127e (Initial commit)
