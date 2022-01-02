cc22937a05473af2a7aa2152c9863de9e311c0a8
RINONIN <779057748@qq.com>
2022-01-02 14:44:18 +0800

    Initial commit

--- foundation/file_manager.py
+++ foundation/file_manager.py
@@ -3,3 +3,24 @@
 日期: 2022年01月02日
 时间: 14:31
 """
+# 文件目录操作
+# os.getcwd()
+# os.listdir()
+# os.makedirs()
+# os.path.exists()
+# 文件管理系统
+# os.removedirs()
+# shutil.rmtree()
+# os.rename()
+# 文件目录多种检验
+# os.path.isfile()
+# os.path.exists()
+# os.path.isdir()
+# os.path.basename()
+# os.path.dirname()
+# os.path.split()
+
+import os
+
+print("当前目录：", os.getcwd())
+print("当前目录里有什么：", os.listdir())
